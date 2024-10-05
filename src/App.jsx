998b17079325f657a6e381460f1859806dc521dd
Muhammad Ahmed <153613965+Muhammadahmedanis@users.noreply.github.com>
2024-10-05 23:42:33 +0500

    header

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Components/Header'
+import Home from './Components/Home'
 
 function App() {
 
   return (
     <>
-     
+        <Header />
+        <Home />
     </>
   )
 }
